5f375e426a8bf2b799f2f55b02c04055b4a78ace
anyasidr <43804913+anyasidr@users.noreply.github.com>
2018-10-30 20:14:55 +0500

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -51,8 +51,55 @@ class Test(unittest.TestCase):
         self.assertEqual(result[4].position, 35)
 
         
+    #unittest for method gen_tokenize
+    def gen_test_type_input_notlist(self):
+        with self.assertRaises(ValueError):
+            self.Tokenizer.gen_tokenize(['eto', 'ne', 'spisok'])
+
+    def gen_test_type_input_number(self):
+        with self.assertRaises(ValueError):
+            self.Tokenizer.gen_tokenize(5)
+
+    def gen_test_result_words(self):
+        result = self.Tokenizer.gen_tokenize('we ^&* are testing- *&$^ this thing')
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].text, 'we')
+        self.assertEqual(result[0].position, 0)
+        self.assertEqual(result[4].text, 'thing')
+        self.assertEqual(result[4].position, 30)
+
+    def gen_test_result_characters_beginning(self):
+        result = self.Tokenizer.gen_tokenize('$%$we ^&* are testing- *&$^ this thing')
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].text, 'we')
+        self.assertEqual(result[0].position, 3)
+        self.assertEqual(result[4].text, 'thing')
+        self.assertEqual(result[4].position, 33)
+
+    def gen_test_result_characters_end(self):
+        result = self.Tokenizer.gen_tokenize('we ^&* are testing- *&$^ this thing()(')
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].text, 'we')
+        self.assertEqual(result[0].position, 0)
+        self.assertEqual(result[4].text, 'thing')
+        self.assertEqual(result[4].position, 30)
+
+    def gen_test_result_characters_begin_end(self):
+        result = self.Tokenizer.gen_
+        tokenize('720@!we ^&* are testing- *&$^ this thing*%@3')
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].text, 'we')
+        self.assertEqual(result[0].position, 5)
+        self.assertEqual(result[4].text, 'thing')
+        self.assertEqual(result[4].position, 35)
+        
+        
+
+
 if __name__ == '__main__':
     unittest.main()
+            
+
             
 
     
