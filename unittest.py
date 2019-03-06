fea4b8e5cb010fc8a1f535bfdd94cba6a9a1dd56
anyasidr <43804913+anyasidr@users.noreply.github.com>
2019-03-06 11:39:48 +0300

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -185,18 +185,12 @@ class TestSearchEngine(unittest.TestCase):
         for filename in os.listdir(os.getcwd()):            
             if filename == 'database' or filename.startswith('database.'):
                 os.remove(filename)        
-        if 'test1.txt' in os.listdir(os.getcwd()):
-            os.remove('test.txt')
-        if 'test2.txt' in os.listdir(os.getcwd()):
-            os.remove('tst.txt')
         
         
 if __name__ == '__main__':
     unittest.main()
         
-        
-if __name__ == '__main__':
-    unittest.main()
+
             
 
     
