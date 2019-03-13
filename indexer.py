4a30cee7f1ec54876553eaebcaa991659834bb78
anyasidr <43804913+anyasidr@users.noreply.github.com>
2019-03-13 09:53:41 +0300

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -4,47 +4,106 @@ import os
 
 
 class Position(object):
+    """
+    This class contains positions of tokens that are
+    alphas and digits. Positions consist of beginnings
+    and endings of tokens.
+    """
+  
     def __init__(self, start, end):
+        """
+        method that creates an example of
+        class Position.
+        """
         self.start = start
         self.end = end
 
     def __eq__(self, obj):
+        """
+        method that compares tokens by their
+        initial and final positions.
+        """
         return self.start == obj.start and self.end == obj.end
 
     def __repr__(self):
+        """
+        This method provides an appropriate 
+        representation.
+        """
         return '(' + str(self.start) + ';' + ' ' + str(self.end) + ')'
 
 
 class Position_with_lines(object):
+    """
+    This class contains positions of the first and 
+    last symbol of each token and also the number
+    of its line.
+    """
+    
     def __init__(self, start, end, line):
+        """
+        method that creates an example of
+        class Position_with_lines.
+        """
         self.start = start
         self.end = end
         self.line = line
 
     def __eq__(self, obj):
+        """
+        method that compares tokens by their
+        initial and final positions and also 
+        by their number of lines.
+        """
         return (self.start == obj.start and self.end == obj.end and
                 self.line == obj.line)
 
     def __repr__(self):
+        """
+        This method provides an appropriate 
+        representation.
+        """
         return '(' + str(self.start) + ',' + ' ' + str(self.end) + ',' + str(self.line) + ')'
 
 
 class Indexator(object):
+    """
+    This class is used for indexing text.
+    Indexing means to create a database that will
+    contain positions of all tokens in given text. 
+    """
+    
     def __init__(self, db_name):
+        """
+        method that creates an example
+        of class Indexator.
+        """
         self.database = shelve.open(db_name, writeback=True)
 
     def indextie(self, filename):
+        """
+        This method indexties text that is stored
+        in some file. The method opens the file,
+        indexties the text and puts all tokens with their
+        positions in a database.
+        """
         if not isinstance(filename, str):
             raise TypeError('Inappropriate type')
         text = open(filename)
         tokenizer = Tokenizer()
-        for word in tokenizer.for_index_tokenize(text.read()):
+        for word in tokenizer.for_index_tokenize(text.read()): 
             self.database.setdefault(word.text, {}).setdefault(filename, []).append(Position(word.position,
             (word.position + len(word.text))))
         text.close()
         self.database.close()
 
     def indextie_with_lines(self, filename):
+        """
+        This method indexties text that is stored
+        in some file. The method opens the file,
+        indexties the text and puts all tokens with their
+        positions and number of the line of in a database.
+        """
         if not isinstance(filename, str):
             raise TypeError('Inappropriate type')
         text = open(filename)
@@ -57,6 +116,9 @@ class Indexator(object):
         self.database.close()
 
     def __del__(self):
+        """
+        the method closes our database.
+        """
         self.database.close()
 
 def main():
