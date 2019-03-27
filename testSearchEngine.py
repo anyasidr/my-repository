0a367cef7aed8b502f4df418606c6403dc46d8ac
anyasidr <43804913+anyasidr@users.noreply.github.com>
2019-03-27 12:15:08 +0300

    Update testSearchEngine.py

--- testSearchEngine.py
+++ testSearchEngine.py
@@ -6,31 +6,9 @@ from indexer import Indexator, Position, Position_with_lines, SearchEngine
 
 class TestSearchEngine(unittest.TestCase):
     def setUp(self):
-        index = indexer.Indexer('database')        
-        file1 = open('test1.txt', 'w')
-        file1.write('this is\ntest')
-        file1.close()
-        file2 = open('test2.txt', 'w')
-        file2.write('test')
-        file2.close()        
-        index.indextie_with_lines('test1.txt')
-        index.indexitie_with_lines('test2.txt')
-        del index
-        self.s = SearchEngine('database')
+       
 
-    def test_empty(self):
-        result = self.s.search('')
-        self.assertEqual(result, {})
-
-    def test_search(self):
-        result = self.s.search('test')
-        self.assertEqual(result, {'test1.txt': [Position_with_lines(0, 4, 1)],
-                                  'test2.txt': [Position_with_lines(0, 4, 0)]})
-
-    def test_search_many(self):
-        result = self.s.search_many('test')
-        self.assertEqual(result, {'test1.txt': [Position_with_lines(0, 4, 1)],
-                                  'test2.txt': [Position_with_lines(0, 4, 0)]})
+    
 
     def tearDown(self):
         del self.s
