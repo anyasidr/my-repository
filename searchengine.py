66aab8c61eb9d5cdf3207b854b4457a597f00b16
anyasidr <43804913+anyasidr@users.noreply.github.com>
2019-06-18 01:03:39 +0500

    Update searchengine.py

--- searchengine.py
+++ searchengine.py
@@ -2,132 +2,10 @@ import shelve
 import os
 import indexer
 import re
+import windows
 from moytokenizer import Tokenizer
 from indexer import Position_with_lines
 
-class ContextWindow(object):
-    """
-    This class is used to store context windows data
-
-    position: list of positions of words for context window
-    line: string that contains the word for context
-    start: position of the first character of the context window
-    end: position after the last character of the context window
-    """
-    def __init__(self, line, position, start, end):
-        self.line = line
-        self.position = position
-        self.start = start
-        self.end = end 
-
-    @classmethod
-    def load_from_file(cls, filename, position, size):
-        """
-        Creates an instance of class ContextWindow from file.
-
-        @param filename: path to the file with the word
-        @param position: position of the searching word in context window
-        @param size: size of the context window
-        """
-        t = Tokenizer()
-        if not (isinstance(filename, str)
-                and isinstance(position, Position_with_lines)
-                and isinstance(size, int)):
-            raise ValueError (filename, position, size)
-
-        with open(filename) as f:
-            for i, line in enumerate(f):
-                if i == position.line:
-                    break
-        if i != position.line:
-            raise ValueError('Wrong line number')
-        line = line.strip("\n")
-        positions = [position]        
-        right = line[position.start:]
-        left = line[:position.end][::-1]
-        
-        for i, token in enumerate(t.for_index_tokenize(left)):
-            if i == size:
-                break
-        start = position.end - token.position - len(token.text)
-        for i, token in enumerate(t.for_index_tokenize(right)):
-            if i == size:
-                break
-        end = position.start + token.position + len(token.text)
-        return cls(line, positions, start, end)
-
-    def is_cross(self, wnd):
-        """
-        Check cross of two context windows
-
-        @param wnd: context window to check
-        """
-        return (self.start <= wnd.end and
-                self.end >= wnd.start and
-                wnd.line == self.line)
-
-    def join_context(self, wnd):
-        """
-        Join context windows and set it to self
-
-        @param wnd: context window to join
-        """
-        for position in wnd.position:
-            if position not in self.position:
-                self.position.append(position)
-        self.start = min(self.start, wnd.start)
-        self.end = max(self.end, wnd.end)
-
-    def expand_context(self):
-        """
-        Expand context window to sentence
-        """
-
-        first = re.compile(r'[.!?]\s[A-ZА-Яa-zа-я]')
-        last = re.compile(r'[A-ZА-Яa-zа-я]\s[.!?]')
-
-        right = self.line[self.end:]
-        left = self.line[:self.start+1][::-1]    
-        if left:
-            try:
-                self.start = self.start - last.search(left).start()
-            except:
-                pass
-        if right:
-            try:
-                self.end += first.search(right).start() + 1
-            except:
-                pass
-
-    def highlight(self):
-        """
-        Creates a string with highlighted words in search query
-        """
-        highlighted = self.line[self.start:self.end]
-        for pos in self.position[::-1]:
-            end = pos.end - self.start
-            start = pos.start - self.start
-            highlighted = highlighted[:end] + '</strong>' + highlighted[end:]
-            highlighted = highlighted[:start] + '<strong>' + highlighted[start:]
-        return highlighted
-        
-    def __eq__(self, wnd):
-        """
-        Check if two context windows are equal
-
-        @param wnd: context window to check
-        """
-        return ((self.position == wnd.position) and
-                (self.line == wnd.line) and
-                (self.start == wnd.start) and
-                (self.end == wnd.end))
-
-    def __repr__(self):
-        """
-        Represents ContextWindow instance to string
-        """
-        return str(self.position)+ ', ' + str(self.start)+ ', ' \
-               + str(self.end)+ ', ' + self.line
 
 class SearchEngine(object):
     """
@@ -187,15 +65,13 @@ class SearchEngine(object):
                 isinstance(size, int)):
             raise ValueError
         
-        contexts_dict = {}
+        conts_dict = {}
         for f, positions in in_dict.items():
-            
             for position in positions:
-                context = ContextWindow.load_from_file(f, position, size)
-                contexts_dict.setdefault(f, []).append(context)
-
-        joined_contexts_dict = self.join_windows(contexts_dict)
-        return joined_contexts_dict
+                cont = windows.ContextWindow.find_window(f, position, size)
+                conts_dict.setdefault(f, []).append(cont)
+        joined_conts_dict = self.join_windows(conts_dict)
+        return joined_conts_dict
 
     def join_windows(self, in_dict):
         """
@@ -203,28 +79,27 @@ class SearchEngine(object):
 
         @param in_dict: dict to join
         """
-        contexts_dict = {}
-        null = ContextWindow([], "", 0, 0)
-        for f, contexts in in_dict.items():
-            previous_context = null
-            for context in contexts:
-                if previous_context.is_cross(context):
-                    previous_context.join_context(context)
+        conts_dict = {}
+        empty = windows.ContextWindow([], "", 0, 0)
+        for f, conts in in_dict.items():
+            previous_cont = empty
+            for cont in conts:
+                if previous_cont.is_cross(cont):
+                    previous_cont.join_cont(cont)
                 else:
-                    if previous_context is not null:
-                        contexts_dict.setdefault(f, []).append(previous_context)
-                    previous_context = context
-            contexts_dict.setdefault(f, []).append(previous_context)
-
-        return contexts_dict
+                    if previous_cont is not empty:
+                        conts_dict.setdefault(f, []).append(previous_cont)
+                    previous_cont = cont
+            conts_dict.setdefault(f, []).append(previous_cont)
+        return conts_dict
 
     def search_to_window(self, query, size=3):
         """
         Search query words in database
         """
         positions_dict = self.search_many(query)
-        context_dict = self.get_window(positions_dict, size)
-        return context_dict
+        cont_dict = self.get_window(positions_dict, size)
+        return cont_dict
 
     def search_to_sentence(self, query, size=3):
         """
@@ -244,9 +119,9 @@ class SearchEngine(object):
         """
         sentence_dict = self.search_to_sentence(query, size)
         quote_dict = {}
-        for f, contexts in sentence_dict.items():
-            for context in contexts:
-                quote_dict.setdefault(f, []).append(context.highlight())
+        for f, conts in sentence_dict.items():
+            for cont in conts:
+                quote_dict.setdefault(f, []).append(cont.highlight())
         return quote_dict
 
     def close(self):
